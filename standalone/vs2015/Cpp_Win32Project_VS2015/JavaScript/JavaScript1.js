f58c3538ac03a7c368e8279ed3a7e15f2010360a
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-03-08 11:13:04 +0000

    Added standalone C++ sample projects

---
+++ standalone/vs2015/Cpp_Win32Project_VS2015/JavaScript/JavaScript1.js
@@ -0,0 +1,27 @@
+﻿var a1 = b + c; // This is a trailing comment that can be very very long
+
+function doSomething(param1, param2, param3, param4, param5) {
+ //...
+}
+
+
+
+function f() {
+    iiiiiii = 1;         // Noncompliant; i is global
+
+    for (jjjj = 0; j < array.length; jjjj++) {  // Noncompliant; j is global now too
+        // ...
+    }
+}
+
+
+function say(a, b) {
+    print(a + " " + b);
+}
+
+say("hello", "world", "!"); // Noncompliant; last argument is not used
+
+
+for (; ;) {  // Noncompliant; end condition omitted
+    // ...
+}
