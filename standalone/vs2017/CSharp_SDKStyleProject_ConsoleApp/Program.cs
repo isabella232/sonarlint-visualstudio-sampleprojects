96601188da1159daeee70835399a7c78fa9b98af
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-03-08 11:48:26 +0000

    Added standalone C# and VB.NET projects (old- and new-style projects)

---
+++ standalone/vs2017/CSharp_SDKStyleProject_ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp_SDKStyleProject_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ standalone/vs2019/VB_OldStyleProject_ClassLibrary/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VB_OldStyleProject_ClassLibrary")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("VB_OldStyleProject_ClassLibrary")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("85f91f18-3920-4f2f-8fcd-67756655c3b8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
