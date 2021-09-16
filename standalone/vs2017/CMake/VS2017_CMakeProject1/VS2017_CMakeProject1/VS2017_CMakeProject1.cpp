6c5821370e975a5d8679e48c64b42e896a1c960a
dp <dpcode@outlook.com>
2021-09-16 16:12:49 +0100

    Added simple CMake projects for VS2017 and VS2019

---
+++ standalone/vs2017/CMake/VS2017_CMakeProject1/VS2017_CMakeProject1/VS2017_CMakeProject1.cpp
@@ -0,0 +1,12 @@
+﻿// VS2017_CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "VS2017_CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ standalone/vs2019/CMake/VS2019_CMakeProject1/VS2019_CMakeProject1.cpp
@@ -0,0 +1,12 @@
+﻿// VS2019_CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "VS2019_CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
