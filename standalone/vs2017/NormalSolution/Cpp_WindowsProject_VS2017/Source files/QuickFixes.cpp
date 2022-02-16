4da565360899dbb41ea2322d0e7a464ff3055268
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-02-16 11:32:46 +0100

    Added comments for quick fixes

--- standalone/vs2017/NormalSolution/Cpp_WindowsProject_VS2017/Source files/QuickFixes.cpp
+++ standalone/vs2017/NormalSolution/Cpp_WindowsProject_VS2017/Source files/QuickFixes.cpp
@@ -5,10 +5,18 @@ class BaseException { };
 class DerivedException : public BaseException { };
 class DerivedException2 : public BaseException { };
 
-typedef int x1;
+/*
+* Expected Fixes
+* * Replace by a type-alias syntax
+*/
+typedef int x1; 
 int main()
 {
-
+	/*
+	* Expected Fixes
+	* * Change to pointer-to-const,
+	* * Replace by nullptr
+	*/
 	int* a = NULL;
 	cout << "Hello CMake." << endl;
 	//int a;
@@ -18,14 +26,31 @@ int main()
 	{
 		// ...
 	}
+	/*
+	* Expected Fixes
+	* * Change to reference-to-const,
+	*/
 	catch (BaseException& b) // Will catch DerivedException as well
 	{
 		// ...
 	}
+	/*
+	* Expected Fixes
+	* * Swap "DerivedException" and "BaseException" handlers
+	* * Remove the dead handler
+	* * Change to reference-to-const,
+	*/
 	catch (DerivedException& d) // Noncompliant, the previous handled effectively hides this one
 	{
 		// Any code here will be unreachable,
 	}
+	/*
+	* Expected Fixes
+	* * Move "BaseException" handler after the more spesific "DerivedException2"
+	* * Move "DerivedException2" handler after the more generic "BaseException"
+	* * Remove the dead handler
+	* * Change to reference-to-const,
+	*/
 	catch (DerivedException2& d) // Noncompliant, the previous handled effectively hides this one
 	{
 		// Any code here will be unreachable,
