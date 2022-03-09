681a066ef6bf57b938e76d774fdf931d8d97df2d
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-03-09 16:41:10 +0000

    [Bound 2019] Added C# and VB.NET files with issues to be suppressed

---
+++ bound/sonarcloud/SLVS_Samples_Bound_VS2019/Taint_CSharp_NetCore_WebAppReact/Suppressions_CSharp.cs
@@ -0,0 +1,17 @@
+﻿namespace Taint_CSharp_NetCore_WebAppReact
+{
+    // TODO 1 : S1135 - not suppressed
+
+    // TODO 2: S1135 - suppressed on server
+
+    public class Suppressions_CSharp
+    {
+        public void EmptyMethod1() // S1186 - not suppressed 
+        {
+        }
+
+        public void EmptyMethod2() // S1186 - suppressed on server
+        {
+        }
+    }
+}
---
+++ bound/sonarcloud/SLVS_Samples_Bound_VS2019/VB_NetCore_ClassLibrary/Suppressions_VB.vb
@@ -0,0 +1,13 @@
+﻿Public Class Suppressions_VB
+
+    REM TODO 1 S1135 - not suppressed
+
+    REM TODO 2 S1135 - suppressed on server
+
+    Public Sub EmptyMethod1()     ' S1186 - not suppressed
+    End Sub
+
+    Public Sub EmptyMethod2()     ' S1186 - suppressed on server
+    End Sub
+
+End Class
