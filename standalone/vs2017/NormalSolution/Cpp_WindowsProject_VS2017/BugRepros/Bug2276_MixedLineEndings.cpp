c5e26228dddff68c1eccf5d44632f7ebd17df38e
Uğraş Ergün <ugras.ergun@sonarsource.com>
2022-04-27 15:37:11 +0200

    dropped 2015-2017

--- standalone/vs2017/NormalSolution/Cpp_WindowsProject_VS2017/BugRepros/Bug2276_MixedLineEndings.cpp
+++
@@ -1,9 +0,0 @@
-// NOTE: do NOT normalize the line endings for this file - needed to repro the bug
-// https://github.com/SonarSource/sonarlint-visualstudio/issues/2276
-
-#include <iostream>
-
-int main()
-{
-	std::cout << "text" << std::endl;
-}
--- standalone/vs2017/NormalSolution/Cpp_WindowsProject_VS2017/Source files/v6_27_S6200.cpp
+++
@@ -1,5 +0,0 @@
-int volatile f(int volatile i); // Noncompliant, both for the return type and the parameter
-
-void g() {
-	auto volatile[a, b] = getPair(); // Noncompliant
-}
