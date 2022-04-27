4a6a6e1a88c6d01d92c0e913abe41c2ad6c595b5
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-04-27 17:43:53 +0100

    Added test files for C# S1172

---
+++ standalone/vs2019/NormalSolution/CSharp_SDKStyleProject_ConsoleApp/Rules/S1172Bug_fixed_v8_38.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CSharp_SDKStyleProject_ConsoleApp.Rules
+{
+    // RSPEC examples:
+    // https://sonarsource.github.io/rspec/#/rspec/S1172/csharp
+
+    public class RspecS1172NonCompliant
+    {
+
+        private void DoSomething(int a, int b) // "b" is unused
+        {
+            Compute(a);
+        }
+
+        private void DoSomething2(int a) // value of "a" is unused
+        {
+            a = 10;
+            Compute(a);
+        }
+        private static void Compute(int a) { Console.WriteLine(a); }
+    }
+
+    public class RspecS1172Compliant
+    {
+        private void DoSomething(int a)
+        {
+            Compute(a);
+        }
+
+        private void DoSomething2()
+        {
+            var a = 10;
+            Compute(a);
+        }
+
+        private static void Compute(int a) { Console.WriteLine(a); }
+    }
+
+    // Example from bug: https://github.com/SonarSource/sonar-dotnet/issues/5491
+    public class Class1
+    {
+        public static void Main(string[] args) // Should not see S1172 here
+        {
+            new Class1().DoSth("Test");
+        }
+
+        private void DoSth(string param) // Should not see S1172 here
+        {
+            Console.Out.WriteLine(param);
+        }
+    }
+}
