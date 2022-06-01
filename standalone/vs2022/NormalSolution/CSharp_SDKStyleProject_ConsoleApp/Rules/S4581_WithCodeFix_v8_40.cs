f0f6308deda1b6a35423b5f8b6ff3fee203ab005
duncanp-sonar <duncan.pocklington@sonarsource.com>
2022-06-01 17:27:02 +0100

    Added C#/VB S4581 examples to VS2022 standalone

---
+++ standalone/vs2022/NormalSolution/CSharp_SDKStyleProject_ConsoleApp/Rules/S4581_WithCodeFix_v8_40.cs
@@ -0,0 +1,13 @@
+﻿namespace CSharp_SDKStyleProject_ConsoleApp.Rules
+{
+    internal class S4581_WithCodeFix_v8_40
+    {
+        public void Foo()
+        {
+            var g1 = new Guid();    // Noncompliant - what's the intent?
+            Guid g2 = new();        // Noncompliant
+            var g3 = default(Guid); // Noncompliant
+            Guid g4 = default;      // Noncompliant
+        }
+    }
+}
---
+++ standalone/vs2022/NormalSolution/VB_OldStyleProject_ClassLibrary/Rules/S4581_v8_40.vb
@@ -0,0 +1,14 @@
+﻿Public Class S4581_v8_40
+
+    Public Sub Foo()
+        Dim G1 As New Guid        ' Noncompliant - what's the intent?
+        Dim G2 As Guid = Nothing  ' Noncompliant
+    End Sub
+
+    Public Sub Foo(Bytes As Byte())
+        Dim G1 As Guid = Guid.Empty
+        Dim G2 As Guid = Guid.NewGuid()
+        Dim G3 As Guid = New Guid(Bytes)
+    End Sub
+
+End Class
